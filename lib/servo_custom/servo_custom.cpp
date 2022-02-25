6bffd0e094f499d84b1197c20a219031b9772f09
Carson-Bay <cgbman02@gmail.com>
2022-02-24 20:56:30 -0500

    servo wrapper

---
+++ lib/servo_custom/servo_custom.cpp
@@ -0,0 +1,38 @@
+#include "Arduino.h"
+#include <Servo.h>
+
+
+// Constructors
+Servo_c::Servo_c(){
+	this->servo = Servo servo;
+	this->min_range = 0;
+	this->max_range = 300;
+};
+
+// Parametric constructor
+Servo_c::Servo_c(Servo servo, int min_range, int max_range){
+	this->servo = servo;
+	this->min_range = min_range;
+	this->max_range = max_range;
+};
+
+// Accessors
+double Servo_c::read() const {
+	double angle = this->servo.read();
+	double converted_angle = 300 * angle / 180;
+	return converted_angle;
+};
+
+// Mutators
+bool Servo_c::write(double angle){
+	if(angle >= this->min_range && angle <= this->max_range){
+		int microseconds = round(((2000/300) * angle) + 500);
+		this->servo.writeMicroseconds(microseconds);
+		return true;
+	}
+	else{return false;}
+};
+
+void Servo_c::attach(uint pin){
+	this->servo.attach(pin);
+};
---
+++ lib/servo_custom/servo_custom.h
@@ -0,0 +1,32 @@
+#ifndef SERVO_H
+#define SERVO_H
+
+#include "Arduino.h"
+#include <Servo.h>
+
+class Servo_c {
+
+private:
+
+	Servo servo;
+	int min_range;
+	int max_range;
+
+public:
+	// Constructors
+	Servo_c();
+
+	// Parametric constructor
+	Servo_c(Servo servo, int min_range, int max_range);
+
+	// Accessors
+	double read() const;
+
+	// Mutators
+	bool write(double angle);
+
+	void attach(uint pin);
+};
+
+
+#endif
